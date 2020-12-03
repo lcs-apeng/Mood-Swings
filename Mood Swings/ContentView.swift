0ee4e29ad46913116a2d9e999756e99ac5fe3bc1
ameliapeng <apeng@lcs.on.ca>
2020-12-03 09:05:38 -0500

    Initial Commit

---
+++ Mood Swings/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Mood Swings
+//
+//  Created by amelia peng on 2020-12-03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Mood Swings/Mood_SwingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mood_SwingsApp.swift
+//  Mood Swings
+//
+//  Created by amelia peng on 2020-12-03.
+//
+
+import SwiftUI
+
+@main
+struct Mood_SwingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
