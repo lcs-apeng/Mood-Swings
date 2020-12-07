2be88e4b90f317b40de2799a8d840b1007799f96
ameliapeng <apeng@lcs.on.ca>
2020-12-07 11:12:31 -0500

    started working on the buttons and adding photos

---
+++ Mood Swings/Child Navigation.swift
@@ -0,0 +1,54 @@
+//
+//  Child Navigation.swift
+//  Mood Swings
+//
+//  Created by amelia peng on 2020-12-05.
+//
+
+import SwiftUI
+
+struct ChildDetailView: View {
+    
+    var child: Child
+    
+    var body: some View {
+        
+        ScrollView {
+
+            VStack {
+                
+                // NOTE: If no image appears, be sure an image with the appropriate name is in the Assets.xcassets folder
+                Text("Showing image named \(child.imageName)... IF it is in the Assets.xcassets folder")
+                
+                Image(child.imageName)
+                        .resizable()
+                        .scaledToFit()
+                
+                Text(child.description)
+                
+            }
+
+        }
+        .navigationTitle(child.name)
+        
+    }
+}
+
+struct ChildDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        
+        NavigationView {
+            
+            ChildDetailView(child: Child(name: "Option 1a",
+                                         description: """
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut tristique porttitor odio ut iaculis. Aliquam aliquet, sem sed imperdiet tincidunt, nibh urna tristique quam, sed tristique risus ex et elit. Proin tellus nulla, semper vel rutrum sit amet, imperdiet a orci. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed a faucibus leo. In hac habitasse platea dictumst. Sed nec ex vel nulla convallis venenatis. Duis pretium sit amet nunc eget suscipit. Nam vitae velit sit amet velit posuere tincidunt in at tortor. Integer congue velit erat, ut rutrum lectus aliquam id. Praesent ac ipsum in turpis semper vestibulum non at eros. Curabitur placerat, odio in vehicula pretium, ante tortor porttitor orci, efficitur viverra nisi est ac tortor.
+                                         
+                                Donec sit amet magna non nisl varius cursus et condimentum urna. Cras elementum maximus nisl. Vivamus hendrerit vitae felis eu interdum. Quisque sit amet nisl dapibus, interdum mauris non, condimentum risus. Integer sit amet ante at enim molestie elementum. Curabitur tempus facilisis magna interdum congue. Ut luctus nulla turpis, et maximus ante blandit ac. Duis gravida dui vel augue pretium placerat convallis lobortis ipsum. Vestibulum congue lacus a massa facilisis ultrices. Nullam at blandit lectus. Cras varius ipsum eget nisi viverra dictum. Nulla eu mollis leo. Phasellus quis nisl tellus. Nulla id aliquet urna.
+                                Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus cursus risus eu elementum volutpat. Fusce pretium odio ac metus interdum interdum. Maecenas auctor leo ac nisi molestie, ut semper lacus vulputate. Etiam id lacinia purus. Maecenas pulvinar augue ullamcorper diam elementum, in dictum sapien consequat. Curabitur lobortis metus eu semper semper. Integer et mauris ut magna malesuada vehicula.
+                                """,
+                                         imageName: "image1a"))
+
+        }
+        
+    }
+}
--- Mood Swings/ContentView.swift
+++ Mood Swings/ContentView.swift
@@ -7,10 +7,103 @@
 
 import SwiftUI
 
+// By conforming to the Identifiable protocol, a type can be iterated over with the List view
+struct Child: Identifiable {
+    var id = UUID()         // Generate a unique identifier for an instance of this type
+    var name: String
+    var description: String
+    var imageName: String
+}
+
+struct Parent {
+    var name: String
+    var children: [Child]
+}
+
 struct ContentView: View {
+    
+    // All top level options
+    var topLevelOptions = [
+        Parent(name: "Happy", children: [
+            
+            Child(name: "Awesome!!",
+                  description: "Thats so good that you're happy! Keep it going!",
+                  imageName: "happypic"),
+            Child(name: "TIRED",
+                  description: "I'm feeling tired...",
+                  imageName: "image1b"),
+            Child(name: "SAD",
+                  description: "I'm kinda sad right now...",
+                  imageName: "image1c"),
+
+        ]),
+        Parent(name: "Tired", children: [
+
+            Child(name: "Option 2a",
+                  description: "I feel tired and sleepy today.",
+                  imageName: "image2a"),
+            Child(name: "Option 2b",
+                  description: "Some explanatory information about option 2b",
+                  imageName: "image2b"),
+
+        ]),
+        Parent(name: "Sad", children: [
+
+            Child(name: "Option 3a",
+                  description: "Some explanatory information about option 3a",
+                  imageName: "image3a"),
+            Child(name: "Option 3b",
+                  description: "Some explanatory information about option 3b",
+                  imageName: "image3b"),
+            Child(name: "Option 3c",
+                  description: "Some explanatory information about option 3c",
+                  imageName: "image3c"),
+            Child(name: "Option 3d",
+                  description: "Some explanatory information about option 3d",
+                  imageName: "image3d"),
+
+        ]),
+    ]
+    
+    // Selected top level option
+    @State var selectedTopLevelOption = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        NavigationView {
+            
+            Form {
+                
+                Section {
+                    
+                    Picker("Select a top level option", selection: $selectedTopLevelOption) {
+                        
+                        ForEach(0 ..< topLevelOptions.count) { index in
+                            
+                            Text(topLevelOptions[index].name)
+                            
+                        }
+                        
+                    }
+
+                }
+                
+                Section {
+                    
+                    // Loop over all the children of the selected top level option
+                    List(topLevelOptions[selectedTopLevelOption].children) { secondLevelOption in
+                        
+                        NavigationLink(secondLevelOption.name,
+                                       destination: ChildDetailView(child: secondLevelOption))
+                        
+                    }
+
+                }
+                
+            }
+            .navigationTitle("Parent-Child Nav")
+        }
+        
     }
 }
 
@@ -19,3 +112,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
